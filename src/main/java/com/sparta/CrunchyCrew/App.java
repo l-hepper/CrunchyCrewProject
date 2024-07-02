553bedd076bd747d55517d74f76067ebfc88bf6a
l-hepper <liamhepper1@gmail.com>
2024-07-02 17:29:22 +0100

    Added logger package with standard logger configuration, logger configuration call added to main, logger added to App class

--- src/main/java/com/sparta/CrunchyCrew/App.java
+++ src/main/java/com/sparta/CrunchyCrew/App.java
@@ -1,7 +1,15 @@
 package com.sparta.CrunchyCrew;
 
-public class App {
-    public static void main(String[] args) {
+import com.sparta.CrunchyCrew.logger.CrunchyLogger;
 
+import java.util.logging.Logger;
+
+public class App {
+
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) {
+        CrunchyLogger.configure();
+        
     }
 }
