18ac17011f319ec1bc027c1cc0006a7bf405a95a
KianDoyleSparta <kdoyle@spartaglobal.com>
2024-07-03 15:31:38 +0100

    Added employeeDAO class outline

---
+++ src/main/java/com/sparta/CrunchyCrew/employeeDAO.java
@@ -0,0 +1,19 @@
+package com.sparta.CrunchyCrew;
+
+public class employeeDAO {
+    public void createEmployee() {
+
+    }
+
+    public void getEmployee() {
+
+    }
+
+    public void updateEmployee() {
+
+    }
+
+    public void deleteEmployee() {
+
+    }
+}
